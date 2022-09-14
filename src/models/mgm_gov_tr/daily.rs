7e4fd6f2124ddc77c8d1fdb3ef13f1741d13830c
A. Taha Baki <atahabaki@pm.me>
2022-09-14 20:41:12 +0300

    ✨ impl ToDailyForecast

--- src/models/mgm_gov_tr/daily.rs
+++ src/models/mgm_gov_tr/daily.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::forecast::{DailyForecast, ToDailyForecast};
+
 pub type DailyResponse = Vec<DailyStat>;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -46,4 +48,56 @@ pub struct DailyStat {
   pub tarih_gun3: String,
   pub tarih_gun4: String,
   pub tarih_gun5: String,
+}
+
+impl ToDailyForecast for DailyStat {
+    fn to_daily_forecast(&self) -> Vec<DailyForecast> {
+        vec![
+            DailyForecast {
+                weather: self.hadise_gun1.into(),
+                lowest_temp: self.en_dusuk_gun1.to_string().into(),
+                highest_temp: self.en_yuksek_gun1.to_string().into(),
+                lowest_humidity: self.en_dusuk_nem_gun1.to_string().into(),
+                highest_humidity: self.en_yuksek_nem_gun1.to_string().into(),
+                wind_speed: self.ruzgar_hiz_gun1.to_string().into(),
+                wind_direction: self.ruzgar_yon_gun1.to_string().into()
+            },
+            DailyForecast {
+                weather: self.hadise_gun2.into(),
+                lowest_temp: self.en_dusuk_gun2.to_string().into(),
+                highest_temp: self.en_yuksek_gun2.to_string().into(),
+                lowest_humidity: self.en_dusuk_nem_gun2.to_string().into(),
+                highest_humidity: self.en_yuksek_nem_gun2.to_string().into(),
+                wind_speed: self.ruzgar_hiz_gun2.to_string().into(),
+                wind_direction: self.ruzgar_yon_gun2.to_string().into()
+            },
+            DailyForecast {
+                weather: self.hadise_gun3.into(),
+                lowest_temp: self.en_dusuk_gun3.to_string().into(),
+                highest_temp: self.en_yuksek_gun3.to_string().into(),
+                lowest_humidity: self.en_dusuk_nem_gun3.to_string().into(),
+                highest_humidity: self.en_yuksek_nem_gun3.to_string().into(),
+                wind_speed: self.ruzgar_hiz_gun3.to_string().into(),
+                wind_direction: self.ruzgar_yon_gun3.to_string().into()
+            },
+            DailyForecast {
+                weather: self.hadise_gun4.into(),
+                lowest_temp: self.en_dusuk_gun4.to_string().into(),
+                highest_temp: self.en_yuksek_gun4.to_string().into(),
+                lowest_humidity: self.en_dusuk_nem_gun4.to_string().into(),
+                highest_humidity: self.en_yuksek_nem_gun4.to_string().into(),
+                wind_speed: self.ruzgar_hiz_gun4.to_string().into(),
+                wind_direction: self.ruzgar_yon_gun4.to_string().into()
+            },
+            DailyForecast {
+                weather: self.hadise_gun5.into(),
+                lowest_temp: self.en_dusuk_gun5.to_string().into(),
+                highest_temp: self.en_yuksek_gun5.to_string().into(),
+                lowest_humidity: self.en_dusuk_nem_gun5.to_string().into(),
+                highest_humidity: self.en_yuksek_nem_gun5.to_string().into(),
+                wind_speed: self.ruzgar_hiz_gun5.to_string().into(),
+                wind_direction: self.ruzgar_yon_gun5.to_string().into()
+            },
+        ]
+    }
 }
