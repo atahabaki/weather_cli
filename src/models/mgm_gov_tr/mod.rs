4c9343b122071fe1701b0b726717f56a363026d1
A. Taha Baki <atahabaki@pm.me>
2022-09-15 18:03:33 +0300

    ✨ new type Hadise, impl ToWeather

--- src/models/mgm_gov_tr/mod.rs
+++ src/models/mgm_gov_tr/mod.rs
@@ -1,3 +1,21 @@
+use crate::forecast::{Weather, ToWeather};
+
 pub mod center;
 pub mod daily;
 pub mod live;
+
+pub type Hadise = String;
+
+impl ToWeather for Hadise {
+    fn to_weather(&self) -> Weather {
+        match self.as_str() {
+            "PB" => Weather::PartiallyCloudy,
+            "A" => Weather::Sunny,
+            "AB" => Weather::Cloudy,
+            "GSY" => Weather::Stormy,
+            "SCK" => Weather::Hot,
+            "SY" => Weather::Rain,
+            _ => Weather::Unknown
+        }
+    }
+}
