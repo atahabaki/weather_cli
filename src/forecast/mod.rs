3e198623c7108d8942f4afaf0c9280d86179fb43
A. Taha Baki <atahabaki@pm.me>
2022-09-15 17:42:19 +0300

    new type: Weather, traits for transformations

--- src/forecast/mod.rs
+++ src/forecast/mod.rs
@@ -10,22 +10,80 @@
 //! - Serve to the same goal, achieve the same user experience.
 
 /// Trait to display the Forecasts.
-pub trait Forecast {
+pub trait Forecast<W> {
   /// Shows daily forecasts.
   fn show_daily_forecast(&self)
   where
-    Self: ToDailyForecast;
+    Self: ToDailyForecast<W>;
   /// Shows live weather status.
   fn show_live_forecast(&self)
   where
-    Self: ToLiveForecast;
+    Self: ToLiveForecast<W>;
+}
+
+/// Enum type for the Weather
+pub enum Weather {
+  /// A sunny day, or clear sky during nighttime.
+  Sunny,
+  /// Partially cloudy weather is when some clouds are hovering above the
+  /// horizon level; however, most of the sky remains clear and there is no precipitation of any kind.
+  PartiallyCloudy,
+  /// Cloudy weather is when a significant amount of clouds is covering the sky
+  /// (at least half the sky).
+  Cloudy,
+  /// Overcast weather is when the sky is completely covered by a cloud blanket.
+  Overcast,
+  /// Rain is the condensed moisture of the atmosphere above falling under the
+  /// form of liquid droplets.
+  Rain,
+  /// Snow is atmospheric water that froze and fell to the ground, covering it.
+  Snow,
+  /// A thunderstorm is a type of weather characterized by lightning.
+  Stormy,
+  /// Fog is literally a cloud at ground level, which raises ambient humidity
+  /// to its maximum, and considerably decreases visibility.
+  Fog,
+  /// Cyclones, hurricanes, and typhoons design the same phenomenon,
+  /// for different parts of the world.
+  Hurricane,
+  /// Really hot weather.
+  Hot,
+  /// May freeze in an instant.
+  Freezing,
+  /// Unknown
+  Unknown,
+}
+
+impl Weather {
+    /// Transforms the Weather enum to emoji.
+    fn to_emoji(&self) -> &str {
+        match self {
+            Weather::Sunny => "☀️",
+            Weather::PartiallyCloudy => "🌤 ",
+            Weather::Cloudy => "🌥 ",
+            Weather::Overcast => "☁️",
+            Weather::Rain => "🌧 ",
+            Weather::Snow => "🌨 ",
+            Weather::Stormy => "🌩 ",
+            Weather::Fog => "🌁",
+            Weather::Hurricane => "🌪️",
+            Weather::Hot => "🌡️",
+            Weather::Freezing => "🥶",
+            Weather::Unknown => "🤷",
+        }
+    }
+}
+
+/// For strange types that needs to be converted into Weather.
+pub trait ToWeather {
+    fn to_weather(&self) -> Weather;
 }
 
 /// A basic DailyForecast type
 /// This is an intermediate data type. Data of this is used to hold weather
 /// stats, et cetera.
-pub struct DailyForecast {
-  pub weather: Option<String>,
+pub struct DailyForecast<W> {
+  pub weather: W,
   pub lowest_temp: Option<String>,
   pub highest_temp: Option<String>,
   pub lowest_humidity: Option<String>,
@@ -35,15 +93,15 @@ pub struct DailyForecast {
 }
 
 /// Convert your type to Vector of DailyForecasts.
-pub trait ToDailyForecast {
-  fn to_daily_forecast(&self) -> Vec<DailyForecast>;
+pub trait ToDailyForecast<W> {
+  fn to_daily_forecast(&self) -> Vec<DailyForecast<W>>;
 }
 
 /// A basic LiveForecast type
 /// This is an intermediate data type. Data of this is used to hold weather
 /// stats, et cetera.
-pub struct LiveForecast {
-  pub weather: Option<String>,
+pub struct LiveForecast<W> where W: ToWeather {
+  pub weather: W,
   pub temperature: Option<String>,
   pub humidity: Option<String>,
   pub wind_speed: Option<String>,
@@ -51,6 +109,6 @@ pub struct LiveForecast {
 }
 
 /// Convert your type to Vector of LiveForecasts.
-pub trait ToLiveForecast {
-  fn to_live_forecast(&self) -> LiveForecast;
+pub trait ToLiveForecast<W> {
+  fn to_live_forecast(&self) -> LiveForecast<W>;
 }
